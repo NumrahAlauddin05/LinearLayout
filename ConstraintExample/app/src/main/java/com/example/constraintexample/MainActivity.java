94cb0e39f12e8dd833ef2d5c9a7a949ab1be9e10
NumrahAlauddin05 <numrah.alauddin@gmail.com>
2021-03-06 04:16:50 -0800

    firebase

--- ConstraintExample/app/src/main/java/com/example/constraintexample/MainActivity.java
+++ ConstraintExample/app/src/main/java/com/example/constraintexample/MainActivity.java
@@ -1,25 +1,82 @@
 package com.example.constraintexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailEt;
+    EditText passEt;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable() {
+        init();
+
+//        Handler handler=new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent =new Intent(MainActivity.this,Main3Activity.class);
+//                startActivity(intent);
+//            }
+//        },5000);
+    }
+
+    private void init() {
+        emailEt = findViewById(R.id.sigupName);
+        passEt = findViewById(R.id.signupPass);
+        auth=FirebaseAuth.getInstance();
+    }
+
+    public void signup(View view) {
+
+        String email = emailEt.getText().toString();
+        String pass = passEt.getText().toString();
+
+        if (email.isEmpty()) {
+            emailEt.setError("Email required");
+        } else if (pass.isEmpty()) {
+            passEt.setError("Password required");
+        } else {
+
+            signupUser(email,pass);
+
+        }
+
+
+    }
+
+    private void signupUser(String email,String pass) {
+
+        auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void run() {
-                Intent intent =new Intent(MainActivity.this,Main3Activity.class);
-                startActivity(intent);
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    Intent intent=new Intent(MainActivity.this,Home.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
             }
-        },5000);
+        });
     }
 }
